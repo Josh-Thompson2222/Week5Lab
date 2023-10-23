3b59e89298d26ad6426031931f1e6882dace1fef
Josh <G00422756@atu.ie>
2023-10-23 16:43:43 +0100

    Creating a new operation to print preset data

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Person student1 = new Person("Josh", "Thompson", 20);
+        System.out.println("Details are " + student1.getFirstName() + " " + student1.getLastName() + " " + student1.getAge());
     }
 }
